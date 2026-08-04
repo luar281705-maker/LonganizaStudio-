9a603ccebdc7560c1dfbec0a514aec5a69ac8a38
luar281705-maker <luar281705@gmail.com>
2026-08-04 01:02:05 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,41 @@
+/*
+========================================
+LONGANIZA STUDIO
+Sistema principal
+Versión 0.1.0
+========================================
+*/
 
+
+console.log(
+    "Longaniza Studio iniciado correctamente"
+);
+
+
+
+const sistema = {
+
+    nombre:
+    "Longaniza Studio",
+
+
+    version:
+    "0.1.0",
+
+
+    proyectoActual:
+    null
+
+
+};
+
+
+
+window.onload = function(){
+
+    console.log(
+        sistema.nombre +
+        " funcionando"
+    );
+
+};
