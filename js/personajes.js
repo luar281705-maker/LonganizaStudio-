71f8248e9d69a51b00af59dad6af1b3fb5921eba
luar281705-maker <luar281705@gmail.com>
2026-08-04 10:17:42 -0600

    Update personajes.js

--- js/personajes.js
+++ js/personajes.js
@@ -72,58 +72,172 @@ contenido.innerHTML = `
 
 
 <h2>
-🎭 Personajes
+🎭 Editor de Personajes
 </h2>
+
+
 
 
 <div class="panel">
 
 
+<h3>
+Información básica
+</h3>
+
+
 <input
-
 id="nombrePersonaje"
-
 placeholder="Nombre del personaje"
-
 >
 
 
-<select id="tipoPersonaje">
 
+<select id="tipoPersonaje">
 
 <option>
 Principal
 </option>
 
-
 <option>
 Secundario
 </option>
-
 
 <option>
 Invitado
 </option>
 
-
 <option>
 Mascota
 </option>
-
 
 </select>
 
 
 
+<input
+id="rolPersonaje"
+placeholder="Rol dentro de la serie"
+>
+
+
+
+<textarea
+id="descripcionPersonaje"
+placeholder="Descripción general"
+></textarea>
+
+
+</div>
+
+
+
+
+
+<div class="panel">
+
+
+<h3>
+Apariencia visual
+</h3>
+
+
+<input
+id="edadPersonaje"
+placeholder="Edad"
+>
+
+
+<input
+id="cabelloPersonaje"
+placeholder="Cabello"
+>
+
+
+<input
+id="colorCabelloPersonaje"
+placeholder="Color de cabello"
+>
+
+
+
+<select id="lentesPersonaje">
+
+<option>
+Sí
+</option>
+
+<option>
+No
+</option>
+
+</select>
+
+
+
+<input
+id="ropaPersonaje"
+placeholder="Ropa habitual"
+>
+
+
+
+<input
+id="accesoriosPersonaje"
+placeholder="Accesorios"
+>
+
+
+</div>
+
+
+
+
+
+
+<div class="panel">
+
+
+<h3>
+Personalidad
+</h3>
+
+
+<textarea
+id="caracteristicasPersonaje"
+placeholder="Características"
+></textarea>
+
+
+<textarea
+id="formaHablarPersonaje"
+placeholder="Forma de hablar"
+></textarea>
+
+
+<textarea
+id="gustosPersonaje"
+placeholder="Gustos"
+></textarea>
+
+
+<textarea
+id="defectosPersonaje"
+placeholder="Defectos"
+></textarea>
+
+
+
 <button onclick="crearPersonaje()">
 
-Crear Personaje
+💾 Guardar Personaje
 
 </button>
 
 
-
 </div>
+
+
 
 
 <hr>
@@ -142,7 +256,6 @@ listarPersonajes();
 
 
 }
-
 
 
 /*
